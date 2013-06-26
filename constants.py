418ed0a702a4878ac4562a4b3f81a5a3015dae88
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2013-06-26 10:41:02 +0200

    First attempt at base for the project

---
+++ constants.py
@@ -0,0 +1,33 @@
+'''Constants used in database'''
+
+# Use large increments to allow for interpolating ranks later
+SUBSPECIES = 0
+SPECIES = 5
+SPECIES_GROUP = 10
+SUBGENUS = 15
+GENUS = 20
+SUBTRIBE = 25
+TRIBE = 30
+SUBFAMILY = 35
+FAMILY = 40
+SUPERFAMILY = 45
+PARVORDER = 50
+INFRAORDER = 55
+SUBORDER = 60
+ORDER = 65
+SUPERORDER = 70
+INFRACLASS = 90
+SUBCLASS = 95
+CLASS = 100
+ROOT = 105
+
+# Nomenclatural group that the taxon belongs to
+GROUP_SPECIES = 0
+GROUP_GENUS = 1
+GROUP_FAMILY = 2
+GROUP_HIGH = 3
+
+# Status of a name
+STATUS_VALID = 0
+STATUS_SYNONYM = 1
+STATUS_DUBIOUS = 2 # nomen dubium, species inquirenda, etcetera
---
+++ database.sql
@@ -0,0 +1,38 @@
+--
+-- TAXONOMY
+--
+
+DROP TABLE IF EXISTS `taxon`;
+CREATE TABLE `taxon` (
+	`id` INT UNSIGNED AUTO_INCREMENT,
+	`valid_name` VARCHAR(512) NOT NULL, -- Current valid name
+	`rank` INT NOT NULL, -- See constants.py
+	`name_id` INT UNSIGNED NOT NULL, -- Id of name that this taxon is based on
+	`comments` VARCHAR(65535) DEFAULT NULL,
+	`data` TEXT DEFAULT NULL, -- Arbitrary data in JSON form
+	PRIMARY KEY(`id`),
+	INDEX(`valid_name`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `name`;
+CREATE TABLE `name` (
+	`id` INT UNSIGNED AUTO_INCREMENT,
+	`taxon_id` INT UNSIGNED, -- Name of valid taxon this belongs to
+	`group` INT NOT NULL, -- Species, genus, family, or higher
+	`status` INT NOT NULL, -- Valid, synonym, or species inquirenda
+	`original_name` VARCHAR(512) DEFAULT NULL,
+	`base_name` VARCHAR(512) DEFAULT NULL,
+	`authority` VARCHAR(1024) DEFAULT NULL,
+	`year` VARCHAR(255) DEFAULT NULL,
+	`page_described` VARCHAR(255) DEFAULT NULL,
+	`original_citation` VARCHAR(512) DEFAULT NULL,
+	`type_id` INT UNSIGNED DEFAULT NULL, -- ID of type genus/species for family, genus group
+	`nomenclature_comments` VARCHAR(65535) DEFAULT NULL,
+	`taxonomy_comments` VARCHAR(65535) DEFAULT NULL,
+	`other_comments` VARCHAR(65535) DEFAULT NULL,
+	`data` TEXT DEFAULT NULL, -- Arbitrary data in JSON form
+	PRIMARY KEY(`id`),
+	INDEX(`original_name`),
+	INDEX(`base_name`),
+	INDEX(`taxon_id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
