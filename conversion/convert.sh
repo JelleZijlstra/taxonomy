dfaa3cfc8dcab26dbbabe31f35acf478d66b52d9
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2017-07-30 13:42:02 -0700

    remove some obsolete code
    Related to spreadsheet conversion and the obsolete web app.

--- conversion/convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-indir=$1
-outdir=$2
-
-for file in $indir/*.ods; do
-	outfile=${file/$indir/$outdir}
-	outfile=${outfile/\.ods/.csv}
-	unoconv -f csv -o $outfile $file
-done
--- conversion/convert_one.sh
+++
@@ -1,1 +0,0 @@
-unoconv -f csv -o $TAXONOMY_ROOT/data/$1.csv ~/Dropbox/export/$1.ods
