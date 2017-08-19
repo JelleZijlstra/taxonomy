cb954fec3dbba96ee31f53597b9adf7213f6d41f
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2017-08-19 15:40:26 -0700

    fix mypy in lib.py

--- taxonomy/lib.py
+++ taxonomy/lib.py
@@ -1,9 +1,13 @@
 import peewee
+from typing import Any, Iterable, List, Optional, Tuple
+
+from .db.constants import Age, Rank
+from .db.models import Location, Name, Occurrence, Period, Taxon
 
 
-def occ(t, loc, source=None, replace_source=False, **kwargs):
+def occ(t: Taxon, loc: Location, source: Optional[str] = None, replace_source: bool = False, **kwargs: Any) -> Occurrence:
     if source is None:
-        source = s
+        source = s  # type: ignore
     try:
         o = t.at(loc)
     except Occurrence.DoesNotExist:
@@ -18,12 +22,13 @@ def occ(t, loc, source=None, replace_source=False, **kwargs):
             print('Replaced source: %s' % o)
     return o
 
-def occur(t, locs, source=None, replace_source=False, **kwargs):
+
+def occur(t: Taxon, locs: Iterable[Location], source: Optional[str] = None, replace_source: bool = False, **kwargs: Any) -> None:
     for loc in locs:
         occ(t, loc, source=source, replace_source=replace_source, **kwargs)
 
 
-def biggest_localities(limit=50):
+def biggest_localities(limit: int = 50) -> List[Tuple[Location, int]]:
     query = Location \
         .select(Location, peewee.fn.Count(Occurrence.id).alias('num_occurrences')) \
         .join(Occurrence, peewee.JOIN_LEFT_OUTER) \
@@ -33,7 +38,7 @@ def biggest_localities(limit=50):
     return list(reversed([(t, t.num_occurrences) for t in query]))
 
 
-def biggest_ranges(limit=50):
+def biggest_ranges(limit: int = 50) -> List[Tuple[Taxon, int]]:
     query = Taxon \
         .select(Taxon, peewee.fn.Count(Occurrence.id).alias('num_occurrences')) \
         .join(Occurrence, peewee.JOIN_LEFT_OUTER) \
@@ -43,40 +48,45 @@ def biggest_ranges(limit=50):
     return list(reversed([(t, t.num_occurrences) for t in query]))
 
 
-def mocc(t, locs, source=None, replace_source=False, **kwargs):
+def mocc(t: Taxon, locs: Iterable[Location], source: Optional[str] = None, replace_source: bool = False, **kwargs: Any) -> None:
     for loc in locs:
         occ(t, loc, source=source, replace_source=replace_source, **kwargs)
 
-def multi_taxon(ts, loc, source=None, replace_source=False, **kwargs):
+
+def multi_taxon(ts: Iterable[Taxon], loc: Location, source: Optional[str] = None, replace_source: bool = False, **kwargs: Any) -> None:
     for t in ts:
         occ(t, loc, source=source, replace_source=replace_source, **kwargs)
 
-def unrecorded_taxa(root):
-    def has_occurrence(taxon):
-        return taxon.occurrences.count()
 
-    if root.age == AGE_FOSSIL:
+def unrecorded_taxa(root: Taxon) -> None:
+    def has_occurrence(taxon: Taxon) -> bool:
+        return taxon.occurrences.count() > 0
+
+    if root.age == Age.fossil:
         return
 
-    if root.rank == SPECIES:
+    if root.rank == Rank.species:
         if not has_occurrence(root) and not any(has_occurrence(child) for child in root.children):
             print(root)
     else:
         for taxon in root.children:
             unrecorded_taxa(taxon)
 
-def move_localities(period):
+
+def move_localities(period: Period) -> None:
     for location in Location.filter(Location.max_period == period, Location.min_period == period):
         location.min_period = location.max_period = None
         location.stratigraphic_unit = period
         location.save()
 
-def move_to_stratigraphy(loc, period):
+
+def move_to_stratigraphy(loc: Location, period: Period) -> None:
     loc.stratigraphic_unit = period
     loc.min_period = loc.max_period = None
     loc.save()
 
-def h(author, year, uncited=False):
+
+def h(author: str, year: str, uncited: bool = False) -> List[Name]:
     query = Name.filter(Name.authority % author, Name.year == year)
     if uncited:
         query = query.filter(Name.original_citation >> None)
