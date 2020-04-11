ac5f1c583aab5ff69b004f14fddd96c0cbc08f4e
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2020-04-11 11:36:10 -0700

    add missing dep, add ability to listen to non-default port

--- hsweb/__main__.py
+++ hsweb/__main__.py
@@ -1,7 +1,11 @@
 from aiohttp import web
+import argparse
 
 from . import index
 
 
 if __name__ == "__main__":
-    web.run_app(index.make_app())
+    parser = argparse.ArgumentParser("hsweb")
+    parser.add_argument("-p", "--port", type=int)
+    args = parser.parse_args()
+    web.run_app(index.make_app(), port=args.port)
--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "mypy",
         "flake8",
         "pytest",
+        "aiohttp",
     ],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
