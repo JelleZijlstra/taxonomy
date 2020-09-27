560c1c749bf6238520e5fb845f2751f11b2f144e
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2020-09-27 19:27:44 +0200

    fix serving react code (hopefully)

--- hsweb/__main__.py
+++ hsweb/__main__.py
@@ -8,6 +8,13 @@ from . import index
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("hsweb")
     parser.add_argument("-p", "--port", type=int)
+    parser.add_argument("-b", "--build-root", type=str)
+    parser.add_argument("-v", "--verbose", action="store_true", default=False)
     args = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
-    web.run_app(index.make_app(), port=args.port)
+
+    if args.verbose:
+        logger = logging.getLogger("peewee")
+        logger.setLevel(logging.DEBUG)
+
+    web.run_app(index.make_app(args.build_root), port=args.port)
--- hsweb/index.py
+++ hsweb/index.py
@@ -1,10 +1,8 @@
 from aiohttp import web
 from functools import lru_cache
 from pathlib import Path
-import taxonomy
-from typing import Callable, Iterator, Type
+from typing import Callable, Type, Optional
 from aiohttp_graphql import GraphQLView
-import logging
 
 from . import components
 from . import view
@@ -13,13 +11,10 @@ from . import schema
 HSWEB_ROOT = Path(view.__file__).parent.parent
 HESPEROMYS_ROOT = Path("/Users/jelle/py/hesperomys")
 
-logger = logging.getLogger("peewee")
-logger.setLevel(logging.DEBUG)
-
 
 @lru_cache()
-def get_static_file_contents(path: str) -> bytes:
-    return (HESPEROMYS_ROOT / "build" / path).read_bytes()
+def get_static_file_contents(hesperomys_dir: Path, path: str) -> bytes:
+    return (hesperomys_dir / "build" / path).read_bytes()
 
 
 def make_handler(
@@ -36,34 +31,15 @@ def make_handler(
 
 
 def make_static_handler(
-    path: str, content_type: str
+    path: str, content_type: str, hesperomys_dir: Path
 ) -> Callable[[web.Request], web.Response]:
     async def handler(request: web.Request) -> web.Response:
         return web.Response(
-            body=get_static_file_contents("index.html"), content_type=content_type
+            body=get_static_file_contents(hesperomys_dir, "index.html"),
+            content_type=content_type,
         )
 
     return handler
-
-
-react_handler = make_static_handler("index.html", "text/html")
-
-
-def get_model_routes() -> Iterator[str]:
-    seen_models = set()
-    for page in view.page.ModelPage.__subclasses__():
-        yield web.get(f"/{page.model_cls.call_sign.lower()}/{{id}}", make_handler(page))
-        seen_models.add(page.model_cls)
-    for model_cls in taxonomy.db.models.BaseModel.__subclasses__():
-        if model_cls not in seen_models:
-            page_cls = type(
-                f"{model_cls.__name__}Page",
-                (view.page.ModelPage,),
-                {"model_cls": model_cls},
-            )
-            yield web.get(
-                f"/{model_cls.call_sign.lower()}/{{id}}", make_handler(page_cls)
-            )
 
 
 async def on_prepare(request: web.Request, response: web.Response) -> None:
@@ -71,15 +47,20 @@ async def on_prepare(request: web.Request, response: web.Response) -> None:
     response.headers["Access-Control-Allow-Headers"] = "*"
 
 
-def make_app() -> web.Application:
+def make_app(build_root: Optional[str] = None) -> web.Application:
+    if build_root is None:
+        hesperomys_dir = HESPEROMYS_ROOT
+    else:
+        hesperomys_dir = Path(build_root)
     app = web.Application()
     GraphQLView.attach(app, schema=schema.schema, graphiql=True)
-    app.router.add_static("/static", HESPEROMYS_ROOT / "build" / "static")
+    app.router.add_static("/static", hesperomys_dir / "build" / "static")
     app.add_routes(
         [web.get("/favicon.ico", make_static_handler("favicon.ico", "image/x-icon"))]
     )
 
     # Delegate everything else to React
+    react_handler = make_static_handler("index.html", "text/html", hesperomys_dir)
     app.add_routes(
         [
             web.get("/{part1}/{part2}/{part3}", react_handler),
@@ -90,6 +71,6 @@ def make_app() -> web.Application:
     )
     app.on_response_prepare.append(on_prepare)
 
-    graphql_schema = HESPEROMYS_ROOT / "hesperomys.graphql"
+    graphql_schema = hesperomys_dir / "hesperomys.graphql"
     graphql_schema.write_text(schema.get_schema_string())
     return app
