9ce2d107051b3a2400338d86d6a15035b5c42f2e
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2023-01-23 21:59:43 -0800

    Add script for concatenating PDFs

---
+++ scripts/concatenate.py
@@ -0,0 +1,58 @@
+"""
+
+Script to concatenate a series of PDFs into one.
+
+Requires GhostScript to be installed.
+
+"""
+import argparse
+from pathlib import Path
+import shlex
+import subprocess
+from taxonomy.config import get_options
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser("concatenate.py")
+    parser.add_argument("directory", help="Directory containing PDFs to concatenate")
+    parser.add_argument("-o", "--output", help="Output filename", default="out.pdf")
+    parser.add_argument(
+        "-F",
+        "--remove-first",
+        help="Remove the first page from each input PDF",
+        action="store_true",
+        default=False,
+    )
+    args = parser.parse_args()
+    options = get_options()
+
+    if args.output:
+        output_path = Path(args.output)
+    else:
+        output_path = Path("out.pdf")
+    output_path = options.new_path / output_path
+
+    inputs = sorted(Path(args.directory).glob("*.pdf"))
+
+    print("Concatenating the following files:")
+    for file in inputs:
+        print(file)
+
+    command = [
+        "gs",
+        "-dBATCH",
+        "-dNOPAUSE",
+        "-q",
+        "-sDEVICE=pdfwrite",
+        f"-sOUTPUTFILE={output_path}",
+    ]
+    if args.remove_first:
+        command.append("-dFirstPage=2")
+    command += [str(p) for p in inputs]
+    print("Running command:")
+    print(" ".join(map(shlex.quote, command)))
+    subprocess.check_call(command)
+
+
+if __name__ == "__main__":
+    main()
