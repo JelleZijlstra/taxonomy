57a2bc401934dce3c8f3304be4e2af8f2ce70190
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2023-02-17 22:40:29 -0800

    add function for generating 5 MB batches

--- taxonomy/db/indexing.py
+++ taxonomy/db/indexing.py
@@ -1,15 +1,19 @@
+import json
 from collections.abc import Iterable
 from typing import Any
 
 from taxonomy.apis.cloud_search import SearchField
 from taxonomy.db.models.base import BaseModel
 
+BATCH_LENGTH_LIMIT = 5 * 1024 * 1024  # 5 MB
+LIMIT_WITH_BUFFER = 0.95 * BATCH_LENGTH_LIMIT
 
-def generate_indexing_requests() -> Iterable[dict[str, Any]]:
+
+def generate_indexing_requests(limit: int | None = None) -> Iterable[dict[str, Any]]:
     for cls in BaseModel.__subclasses__():
         if not cls.search_fields:
             continue
-        for obj in cls.select_valid():
+        for obj in cls.select_valid().limit(limit):
             default_id = f"{cls.call_sign.lower()}/{obj.id}"
             for raw_dict in obj.get_search_dicts():
                 # Filter out null, empty strings, empty lists, etc.
@@ -37,3 +41,24 @@ def generate_field_configs() -> Iterable[dict[str, Any]]:
                 continue
             name_to_field[field.name] = field, cls
             yield field.to_json()
+
+
+def compact_dump(obj: Any) -> str:
+    return json.dumps(obj, separators=(",", ":"), indent=None)
+
+
+def generate_batches(limit: int | None = None) -> Iterable[str]:
+    current_batch: list[str] = []
+    current_batch_length = 0
+    for request in generate_indexing_requests(limit):
+        dumped = compact_dump(request)
+        if current_batch_length + len(dumped) > LIMIT_WITH_BUFFER:
+            yield "[" + ",".join(current_batch) + "]"
+            current_batch = [dumped]
+            current_batch_length = len(dumped)
+        else:
+            current_batch.append(dumped)
+            current_batch_length += len(dumped)
+
+    if current_batch:
+        yield "[" + ",".join(current_batch) + "]"
