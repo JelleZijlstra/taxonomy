37aba58af84e3e2ab6d029207793e9a5fc2ce1f9
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2025-02-10 17:20:20 -0800

    fix CI

--- taxonomy/db/models/issue_date.py
+++ taxonomy/db/models/issue_date.py
@@ -162,5 +162,5 @@ def _get_cgs_with_issue_dates() -> set[int]:
 
 
 IssueDate.creation_event.on(
-    lambda id: _get_cgs_with_issue_dates().add(id.citation_group.id)
+    lambda issue_date: _get_cgs_with_issue_dates().add(issue_date.citation_group.id)
 )
